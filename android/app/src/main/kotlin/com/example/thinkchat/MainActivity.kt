1783350a6dc508303c407bee0e71d38736d30cd7
lupinstarnley <lupinstarnley076@gmail.com>
2026-04-18 11:45:49 +0000

    VEX: Identity Fix - Package renamed to com.vex

--- android/app/src/main/kotlin/com/example/thinkchat/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.thinkchat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/vex/thinkchat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vex.thinkchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
